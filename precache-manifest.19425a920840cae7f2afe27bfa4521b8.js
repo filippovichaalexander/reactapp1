cdace87cbc939747425a2190c90c7b81deafa545
unknown <alex-fil@mail.ru>
2020-12-25 22:40:35 +0300

    Updates

---
+++ precache-manifest.19425a920840cae7f2afe27bfa4521b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d7d0a6880e3ded2e2e0b5fbf120cb2",
+    "url": "/reactapp1/index.html"
+  },
+  {
+    "revision": "b416e4957f81bd4f4c94",
+    "url": "/reactapp1/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "733f548be18b7740a0b0",
+    "url": "/reactapp1/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "b416e4957f81bd4f4c94",
+    "url": "/reactapp1/static/js/2.256e093e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp1/static/js/2.256e093e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "733f548be18b7740a0b0",
+    "url": "/reactapp1/static/js/main.d725eb08.chunk.js"
+  },
+  {
+    "revision": "e17329de7da15c5d6e99",
+    "url": "/reactapp1/static/js/runtime-main.98706691.js"
+  }
+]);
